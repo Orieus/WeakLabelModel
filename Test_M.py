a3d9e7e3a0e12dee7bea552232aed636b83e22eb
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-03-29 13:46:25 +0200

    Solved typo and removed extra spaces

--- Test_M.py
+++ Test_M.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" This code evaluates logistig regression with weak labels
+""" This code evaluates logistic regression with weak labels
     Author: JCS, June, 2016
 """
 
@@ -161,7 +161,7 @@ for k, p in enumerate(pvalues):
     z_bin = wlw.computeVirtual(z, n_classes, method='IPL')
 
     # Target dictionary
-    target = {'true': y, 
+    target = {'true': y,
               'weak': z_bin,
               'virt': v,
               'virtM': v2}
@@ -205,9 +205,9 @@ plt.title('Average error rate')
 for i, tag in enumerate(tag_list):
     plt.plot(pvalues, Pe_cv_mean[tag], label=tag)
 plt.legend()
-plt.xlabel('Parameter of the mixing model')    
-plt.ylabel('Error rate')   
-plt.savefig('PeCV' + datasetName +  optim + '.png') 
+plt.xlabel('Parameter of the mixing model')
+plt.ylabel('Error rate')
+plt.savefig('PeCV' + datasetName +  optim + '.png')
 plt.show(block=False)
 
 # Plot error barplots.
@@ -216,9 +216,9 @@ plt.title('Standard deviations')
 for i, tag in enumerate(tag_list):
     plt.plot(pvalues, Pe_cv_std[tag], label=tag)
 plt.legend()
-plt.xlabel('Parameter of the mixing model')    
-plt.ylabel('Standard deviation')    
-plt.savefig('StdCV' + datasetName + optim + '.png') 
+plt.xlabel('Parameter of the mixing model')
+plt.ylabel('Standard deviation')
+plt.savefig('StdCV' + datasetName + optim + '.png')
 plt.show(block=False)
 
 # ################
