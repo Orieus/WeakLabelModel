5e836599abaa6fb3c53560f2c2c08d6915edafbd
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-04-19 10:29:16 +0100

    Added missing function and passing pep8 standard

--- testUtils.py
+++ testUtils.py
@@ -47,34 +47,46 @@ def get_grid(X, delta=1.0):
     x1_grid = np.arange(x1_min, x1_max, delta)
     x2_grid = np.arange(x2_min, x2_max, delta)
     MX1, MX2 = np.meshgrid(x1_grid, x2_grid)
-    x_grid = np.asarray([MX1.flatten(), MX2.flatten()]).T.reshape(-1,2)
+    x_grid = np.asarray([MX1.flatten(), MX2.flatten()]).T.reshape(-1, 2)
     return x_grid, MX1, MX2
 
 
+def ax_scatter_question(ax, x, notes, zorder=2, clip_on=True, text_size=18,
+                        color='w'):
+    ax.scatter(x[:, 0], x[:, 1], c=color, s=500, zorder=zorder,
+               clip_on=clip_on)
+    for point, n in zip(x, notes):
+        ax.annotate('{}'.format(n), xy=(point[0], point[1]),
+                    xytext=(point[0], point[1]), ha="center", va="center",
+                    size=text_size, zorder=zorder, clip_on=clip_on)
+
+
 def plot_data_predictions(fig, x, y, Z, MX1, MX2, x_predict=None, notes=None,
-                            cmap=cm.YlGnBu, cmap_r=cm.plasma,
-                            loc='best', title=None, aspect='auto', s=30):
+                          cmap=cm.YlGnBu, cmap_r=cm.plasma,
+                          loc='best', title=None, aspect='auto', s=30):
     x1_min, x1_max = MX1.min(), MX1.max()
     x2_min, x2_max = MX2.min(), MX2.max()
 
-    ax = fig.add_subplot(1,1,1)
-    ax.scatter(x[y==0,0], x[y==0,1], c='y', s=s, alpha=0.5, label='Train. Class 1')
-    ax.scatter(x[y==1,0], x[y==1,1], c='b', s=s, alpha=0.5, label='Train. Class 2')
+    ax = fig.add_subplot(111)
+    ax.scatter(x[y == 0, 0], x[y == 0, 1], c='y', s=s, alpha=0.5,
+               label='Train. Class 1')
+    ax.scatter(x[y == 1, 0], x[y == 1, 1], c='b', s=s, alpha=0.5,
+               label='Train. Class 2')
     ax.grid(True)
 
     # Colormap
-    im = ax.imshow(Z, interpolation='bilinear', origin='lower',
-                   cmap=cmap_r, extent=(x1_min, x1_max, x2_min, x2_max),
-                   alpha=0.3, aspect=aspect)
+    ax.imshow(Z, interpolation='bilinear', origin='lower', cmap=cmap_r,
+              extent=(x1_min, x1_max, x2_min, x2_max), alpha=0.3,
+              aspect=aspect)
 
     # Contour
     # FIXME make levels optional
-    #CS = ax.contour(MX1, MX2, Z, levels=[0.01, 0.25, 0.5, 0.75, 0.99],
+    # CS = ax.contour(MX1, MX2, Z, levels=[0.01, 0.25, 0.5, 0.75, 0.99],
     #                cmap=cmap)
     CS = ax.contour(MX1, MX2, Z, cmap=cmap)
     ax.clabel(CS, fontsize=13, inline=1)
-    ax.set_xlim([x1_min,x1_max])
-    ax.set_ylim([x2_min,x2_max])
+    ax.set_xlim([x1_min, x1_max])
+    ax.set_ylim([x2_min, x2_max])
 
     if x_predict is not None:
         ax_scatter_question(ax, x_predict, notes)
