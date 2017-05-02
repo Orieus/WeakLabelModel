d11ee5b093e8a2971f51e53c9ada4e16a251cdb8
Jesús Cid Sueiro <jesus.cid@uc3m.es>
2017-05-02 11:19:57 +0200

    Updated IPL model in computeM

--- wlc/WLweakener.py
+++ wlc/WLweakener.py
@@ -7,7 +7,7 @@ import numpy as np
 import sklearn.datasets as skd           # Needs version 0.14 or higher
 # import sklearn.linear_model as sklm
 import sys
-# import ipdb
+import ipdb
 
 
 def computeM(c, alpha=0.5, beta=0.5, gamma=0.5, method='supervised'):
@@ -16,13 +16,30 @@ def computeM(c, alpha=0.5, beta=0.5, gamma=0.5, method='supervised'):
 
     Parameters
     ----------
-    c : int
-        Number of classes
-    alpha : float, optional (default=0.5)
-    beta : float, optional (default=0.5)
-    gamma : float, optional (default=0.5)
-    method : string, optional (default='supervised')
-        It must be one of 'supervised', 'noisy', 'IPL', 'quasi_IPL'
+    c      : int. Number of classes
+    alpha  : float, optional (default=0.5)
+    beta   : float, optional (default=0.5)
+    gamma  : float, optional (default=0.5)
+    method : string, optional (default='supervised'). Method to compute M.
+             Available options are:
+                'supervised':   Identity matrix. For a fully labeled case.
+                'noisy':        For a noisy label case: the true label is
+                                observed with probabiltity 1 - beta, otherwise
+                                one noisy label is taken at random.
+                'random_noise': All values of the mixing matrix are taken at
+                                random from a uniform distribution. The matrix
+                                is normalized to be left-stochastic
+                'IPL':          Independent partial labels: the observed labels
+                                are independent. The true label is observed
+                                with probability alfa. Each False label is
+                                observed with probability beta.
+                'IPL3':         It is a generalized version of IPL, but only
+                                for c=3 classes and alpha=1: each false label
+                                is observed with a different probability.
+                                Parameters alpha, beta and gamma represent the
+                                probability of a false label for each column.
+                'quasi_IPL':    This is the quasi independent partial label
+                                case discussed in the paper.
 
     Returns
     -------
@@ -41,11 +58,29 @@ def computeM(c, alpha=0.5, beta=0.5, gamma=0.5, method='supervised'):
     elif method == 'random_noise':
 
         # FIXME I thought the rows should sum to one... See the reason
-        M = np.random.rand(c,c)
-        col_sums = M.sum(axis=0)
-        M = M / col_sums[np.newaxis, :]
+        M = np.random.rand(c, c)
+        M = M / np.sum(M, axis=0, keepdims=True)
 
     elif method == 'IPL':
+
+        # Shape M
+        d = 2**c
+        M = np.zeros((d, c))
+
+        # Compute mixing matrix row by row for the nonzero rows
+        for z in range(0, d):
+
+            # Convert the decimal value z to a binary list of length c
+            z_bin = np.array([int(b) for b in bin(z)[2:].zfill(c)])
+            modz = sum(z_bin)
+
+            M[z, :] = (alpha**(z_bin) * (1-alpha)**(1-z_bin) *
+                       (beta**(modz-z_bin) * (1-beta)**(c-modz-1+z_bin)))
+
+        # This is likely not required: columns in M should already sum up to 1
+        M = M / np.sum(M, axis=0)
+
+    elif method == 'IPL3':
 
         M = np.array([
                 [0.0,             0.0,           0.0],
@@ -56,18 +91,6 @@ def computeM(c, alpha=0.5, beta=0.5, gamma=0.5, method='supervised'):
                 [alpha*(1-alpha), 0.0,           gamma*(1-gamma)],
                 [alpha*(1-alpha), beta*(1-beta), 0.0],
                 [alpha**2,        beta**2,       gamma**2]])
-
-    elif method == 'IPL_old':
-
-        M = np.array([
-                [0.0,              0.0,            0.0],
-                [0,                0,              1-gamma-gamma**2],
-                [0,                1-beta-beta**2, 0],
-                [0.0,              beta/2,         gamma/2],
-                [1-alpha-alpha**2, 0,              0],
-                [alpha/2,          0.0,            gamma/2],
-                [alpha/2,          beta/2,         0.0],
-                [alpha**2,         beta**2,        gamma**2]])
 
     elif method == 'quasi_IPL':
 
@@ -91,11 +114,12 @@ def computeM(c, alpha=0.5, beta=0.5, gamma=0.5, method='supervised'):
 
             M[z, :] = z_bin*(beta**(modz-1) * (1-beta)**(c-modz))
 
+        # This is likely not required: columns in M should already sum up to 1
         M = M / np.sum(M, axis=0)
 
     else:
 
-        print "Error: unknown method"
+        print "Error: unknown value of input argumen method in computeM"
         sys.exit()
 
     return M
@@ -116,6 +140,7 @@ def generateWeak(y, M, c):
         z[index] = np.random.choice(dec_labels, 1, p=M[:, i])
 
     return z
+
 
 def computeVirtual(z, c, method='IPL', M=None):
     """
@@ -147,24 +172,34 @@ def computeVirtual(z, c, method='IPL', M=None):
     v = np.zeros((z.size, c))             # virtual labels
 
     for index, i in enumerate(z):         # From dec to bin
+
         z_bin[index, :] = [int(x) for x in bin(int(i))[2:].zfill(c)]
 
     if method == 'IPL' or method == 'supervised':
         # weak and virtual are the same
         pass
+
     elif method == 'quasi_IPL':    # quasi-independent labels
+
         for index, i in enumerate(z_bin):
+
             aux = z_bin[index, :]
             weak_pos = np.sum(aux)
+
             if not weak_pos == c:
+
                 weak_zero = float(1-weak_pos)/(c-weak_pos)
                 aux[aux == 0] = weak_zero
                 z_bin[index, :] = aux
+
             else:
+
                 z_bin[index, :] = np.array([None] * c)
+
     elif method == 'Mproper':
         # Compute the virtual label matrix
         Y = np.linalg.pinv(M)
+
         # Compute the virtual label.
         for index, i in enumerate(z):
             # The virtual label for weak label i is the i-th row in Y
@@ -173,6 +208,7 @@ def computeVirtual(z, c, method='IPL', M=None):
         print 'Unknown method. Weak label taken as virtual'
 
     v = z_bin
+
     return v
 
 
