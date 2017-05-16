39cc6aa715b69430d3f8716399add1bb903636d3
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-05-16 20:17:27 +0100

    Added test to visualize different M, weak labels and virtual labels

--- test_noisy_labels.py
+++ test_noisy_labels.py
@@ -4,28 +4,41 @@ from sklearn.preprocessing import label_binarize
 
 np.random.seed(0)
 
-methods = ['IPL', 'noisy', 'quasi_IPL', 'random_noise', 'random_weak']
-c = 4
-y = np.array([0,1,2,3])
+c = 3
+y = np.array([0,1,2])
 y_bin = label_binarize(y, range(c))
 
-a = 0.99
-b = 0.01
+methods_M = ['IPL', 'noisy', 'quasi_IPL', 'random_noise', 'random_weak']
+shapes = {'IPL':(2**c, c), 'noisy':(c, c), 'quasi_IPL': (2**c, c),
+          'random_noise': (c, c), 'random_weak': (2**c, c)}
 
-for method in methods:
-    print('\n#### ANALYZING M from {} ####'.format(method))
+method_assumptions = ['IPL', 'quasi_IPL', 'Mproper'] # problem with Mproper
+
+a = 0.7
+b = 0.3
+
+for method in methods_M:
+    print('###########################################')
+    print('# ANALYZING M from {} (a={}, b={})'.format(method, a, b))
+    print('###########################################')
     print('True labels are {}'.format(y))
     print('True binary labels are')
     print(y_bin)
     M = wlw.computeM(c, alpha=a, beta=b, method=method)
-    z = wlw.generateWeak(y, M, c)
+    print('M shape = {}'.format(M.shape))
+    assert(M.shape == shapes[method])
+    print(np.round(M,2))
+    z = wlw.generateWeak(y, M)
     print('Weak labels are {}'.format(z))
-    print('Virtual labels are')
-    z_bin = wlw.computeVirtual(z, c, method=method)
+    #print('Weak labels in binary from the old version')
+    #z_bin = wlw.computeVirtual(z, c, method='IPL')
+    #print(z_bin)
+    print('Weak labels in binary are')
+    z_bin = wlw.binarizeWeakLabels(z, c)
     print(z_bin)
-    print('Weak labels in binary from the old version')
-    z_bin = wlw.computeVirtual(z, c, method='IPL')
-    print(z_bin)
-    print('Weak labels in binary from the new version')
-    z_bin = wlw.binarizeWeakLabels(z, c, method=method)
-    print(z_bin)
+    for method_a in method_assumptions:
+        print('---------------------------------------------')
+        print('- Virtual labels assuming M from {} are'.format(method_a))
+        v_bin = wlw.computeVirtual(z, c, method=method_a, M=M)
+        print(np.round(v_bin, 2))
+    print('---------------------------------------------\n')
